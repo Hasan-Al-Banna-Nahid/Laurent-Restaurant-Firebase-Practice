8c87b12b138392524ce5e8e4189dab468d27b6ae
Hasan Al Banna Nahid <iamnahid591998@gmail.com>
2023-05-02 20:11:19 +0600

    Sponsors Section updated

--- src/Components/Sponsor/Sponsor.jsx
+++ src/Components/Sponsor/Sponsor.jsx
@@ -1,28 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./Sponsor.css";
+import Marquee from "react-fast-marquee";
 
 const Sponsor = () => {
   return (
     <div style={{ margin: "50px 0" }}>
       <h3 className="title">Our Sponsors</h3>
-      <div className="sponsors">
-        <div>
-          <img src="../../../public/assests/sponsor1.png" alt="" />
+      <Marquee pauseOnHover={true} className="marquee">
+        <div className="sponsors">
+          <div>
+            <img src="../../../public/assests/sponsor1.png" alt="" />
+          </div>
+          <div>
+            <img src="../../../public/assests/sponsor2.png" alt="" />
+          </div>
+          <div>
+            <img src="../../../public/assests/sponsor3.png" alt="" />
+          </div>
+          <div>
+            <img src="../../../public/assests/sponsor4.png" alt="" />
+          </div>
+          <div>
+            <img src="../../../public/assests/sponsor5.png" alt="" />
+          </div>
         </div>
-        <div>
-          <img src="../../../public/assests/sponsor2.png" alt="" />
-        </div>
-        <div>
-          <img src="../../../public/assests/sponsor3.png" alt="" />
-        </div>
-        <div>
-          <img src="../../../public/assests/sponsor4.png" alt="" />
-        </div>
-        <div>
-          <img src="../../../public/assests/sponsor5.png" alt="" />
-        </div>
-      </div>
+      </Marquee>
     </div>
   );
 };
