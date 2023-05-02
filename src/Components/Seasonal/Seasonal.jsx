38b8481e593dbef2f80acef6a55ae30c80dc1811
Hasan Al Banna Nahid <iamnahid591998@gmail.com>
2023-05-02 17:30:52 +0600

    Featured Section Created

--- src/Components/Seasonal/Seasonal.jsx
+++ src/Components/Seasonal/Seasonal.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import "./Seasonal.css";
 
 const Seasonal = () => {
   return (
@@ -7,9 +8,60 @@ const Seasonal = () => {
       <div>
         <h2 className="title">Seasonal</h2>
         <hr />
-        <h4 className="subtitle">Enjoy Every Moment</h4>
+        <h4 className="subtitle my-4">Enjoy Every Moment</h4>
       </div>
-      <div></div>
+      <div>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+            <div className="card">
+              <img
+                src="../../../public/assests/s1.jpg"
+                className="card-img-top image"
+                alt="..."
+              />
+              <div className="card-body">
+                <div className="d-flex align-items-center justify-content-evenly">
+                  <div>
+                    <h5 className="card-title title">
+                      <sup>$</sup>50
+                    </h5>
+                  </div>
+                  <div>
+                    <h4>Starter</h4>
+                    <p className="card-text subtitle">
+                      Try Our Everyday Starter At Reasonable Price
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img
+                src="../../../public/assests/s2.jpg"
+                className="card-img-top image"
+                alt="..."
+              />
+              <div className="card-body">
+                <div className="d-flex align-items-center justify-content-evenly ">
+                  <div className="price">
+                    <h5 className="card-title title">
+                      <sup>$</sup>100
+                    </h5>
+                  </div>
+                  <div>
+                    <h4>Premium</h4>
+                    <p className="card-text subtitle">
+                      Try Our Holiday Special At Happy Price
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
